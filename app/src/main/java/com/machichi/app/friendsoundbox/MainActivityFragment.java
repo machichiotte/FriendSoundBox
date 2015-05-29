e23520260ccb70ab27374983e94c1044a5953c63
machichiotte <fullplastic59@gmail.com>
2015-05-29 12:40:30 +0200

    ajout png

--- app/src/main/java/com/machichi/app/friendsoundbox/MainActivityFragment.java
+++ app/src/main/java/com/machichi/app/friendsoundbox/MainActivityFragment.java
@@ -1,12 +1,14 @@
 package com.machichi.app.friendsoundbox;
 
 import android.app.Fragment;
+import android.graphics.Color;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 
 /**
@@ -19,8 +21,11 @@ public class MainActivityFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
 
 
-        Button one = (Button)rootView.findViewById(R.id.button_soundbox_A);
+        ImageButton one = (ImageButton)rootView.findViewById(R.id.button_soundbox_A);
+
         final MediaPlayer mp = MediaPlayer.create(getActivity(), R.raw.sonar);
+one.setImageResource(R.drawable.benj);
+        one.setBackgroundColor(Color.TRANSPARENT);
         one.setOnClickListener(new View.OnClickListener(){
 
             public void onClick(View v) {
